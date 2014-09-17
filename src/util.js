15154a6d4f86df8272acd26b3532477ea6ba2c01
Clément LAMOUREUX <clement@raccourci.fr>
2014-09-17 11:08:05 +0200

    margin top in collision

--- src/util.js
+++ src/util.js
@@ -35,7 +35,7 @@ function collision($div1, $div2) {
     var b2 = y2 + h2;
     var r2 = x2 + w2;
 
-    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
+    if (b1 < y2 || (y1+1) > b2 || r1 < x2 || x1 > r2) {
       return false;
     }
     return true;
